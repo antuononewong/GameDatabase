ba5315611f681b4838614290b785b2f4cdc4f7e4
Anthony <48633290+antuononewong@users.noreply.github.com>
2020-10-03 15:17:44 -0500

    Update QuestSurveyHandler.java
    - Creating queries for tailored data
    - Added table name to database schema
    - Added SELECT query on pre-defined queries stored as member variables
    - Added parsing of ResultSet from SELECT query
    - Added function to run every defined query

--- QuestSurveyHandler.java
+++ QuestSurveyHandler.java
@@ -4,12 +4,59 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import GameDatabase.Connection.DBConnection;
 
+/* Script that handles the surveys shown to players upon completion of a quest. The survey will
+ * note basic statistics about the player/location. Survey will be used to gather data that
+ * can be analyzed for improvements upon gameplay/gameplay systems.
+ */
+
+// Database Schema - quest_surveys
+// id (Primary key - int), area (str), quest (str), questType (str), playerLevel (int) difficulty (int), 
+// clearInstructions (int), questLength (int)
+
 public class QuestSurveyHandler {
 	
-	// Database Schema
-	// (area (str), quest (str), questTtype (str), playerLevel (int) difficulty (int), 
-	// clearInstructions (int), questLength (int))
-	// %d - integer, %s string
+	// area, quest - most done quests (shows general pathing of player)
+	// questType, quest - most frequent/enjoyable quests
+	// quest, clearInstrctions - quest comprehension
+	// area, playerLevel, difficulty - general feel of a given area
+	// quest, questLength - general game time on quest
+	// quest, difficulty, clearInstructions, questLength - general feel of a quest
+	// area, playerLevel - completion rate of previous areas
+	
+	private String pathing = "SELECT area, COUNT(quest) AS questCount FROM quest_surveys;";
+	private String questFrequency = "SELECT questType, COUNT(quest) AS questCount FROM quest_surveys;";
+	private String questComprehension = "SELECT quest, AVG(clearInstructions) AS clearAverage FROM quest_surveys;";
+	private String areaFeel = "SELECT area, AVG(playerLevel) AS levelAverage, AVG(difficulty) AS difficultyAverage FROM quest_surveys;";
+	private String questTime = "SELECT quest, AVG(questLength) AS lengthAverage FROM quest_surveys;";
+	private String questFeel = "SELET quest, AVG(difficulty) AS difficultyAverage, AVG(clearInstructions) AS clearAverage, "
+								+ "AVG(questLength) AS lengthAverage FROM quest_surveys;";
+	private String areaCompletion  = "SELECT area, AVG(playerLevel) AS levelAverage FROM quest_surveys;";
+	
+	// Run each defined query and print out the data received from database
+	public void GetAllQuestSurveyData() {
+		String[] pathingColumns = {"area", "questCount"};
+		GetQuestSurveyData(pathing, pathingColumns);
+		
+		String[] questFrequencyColumns = {"questType", "questCount"};
+		GetQuestSurveyData(questFrequency, questFrequencyColumns);
+		
+		String[] questComprehensionColumns = {"quest", "clearAverage"};
+		GetQuestSurveyData(questComprehension, questComprehensionColumns);
+		
+		String[] areaFeelColumns = {"area", "levelAverage" , "difficultyAverage"};
+		GetQuestSurveyData(areaFeel, areaFeelColumns);
+		
+		String[] questTimeColumns = {"quest", "lengthAverage"};
+		GetQuestSurveyData(questTime, questTimeColumns);
+		
+		String[] questFeelColumns = {"quest", "difficultyAverage", "clearAverage", "lengthAverage"};
+		GetQuestSurveyData(questFeel, questFeelColumns);
+		
+		String[] areaCompletionColumns = {"area", "levelAverage"};
+		GetQuestSurveyData(areaCompletion, areaCompletionColumns);
+	}
+	
+	// Adds user-inputed survey data to the database
 	public void AddQuestSurvey(String area, String quest, String questType, int playerLevel,
 							   int difficulty, int clearInstructions, int questLength) {
 		String query = String.format("INSERT INTO quest_surveys (area, quest, questType, playerLevel, "
@@ -26,9 +73,33 @@ public class QuestSurveyHandler {
 		}
 	}
 	
-	public void ParseQuestSurveyData() {
+	// Retrieve data from database with SELECT query
+	private void GetQuestSurveyData(String query, String[] selectColumnNames) {
+		try {
+			ResultSet set = DBConnection.Select(query);
+			if (set != null) {
+				QueryContainer queryContainer = new QueryContainer(query, selectColumnNames);
+				ParseQuestSurveyData(set, queryContainer);
+			}
+		}
+		catch (SQLException e) {
+			System.out.println("---SQLException error from QuestSurveyHandler.GetQuestSurveyData().---");
+			e.printStackTrace();
+		}
 		
 	}
 	
+	// Parse ResultSet from database into a easy to read format
+	private void ParseQuestSurveyData(ResultSet set, QueryContainer queryContainer) throws SQLException {
+		set.next();
+		// ResultSet/SQL queries uses 1 indexing
+		String output = String.format("%s: %s\n", queryContainer.GetColumnName(0), set.getString(1));
+	
+		for (int i = 1; i < queryContainer.GetColumnCount(); i++) {
+			output += String.format("%s: %.2f\n", queryContainer.GetColumnName(i), set.getFloat(i + 1));
+		}
+		
+		System.out.print(output);
+	}
 	
 }
